bf1d9937a4bde9fdcad7bdca0320c12ae542b8d1
mahdikooshkbaghi <mahdi.kooshkbaghi@gmail.com>
2021-09-12 15:47:39 -0400

    change elzar script

--- elzar_submit.sh
+++ elzar_submit.sh
@@ -4,5 +4,5 @@
 #$ -pe threads 16
 #$ -o output
 #$ -e log
-#$ -N chain8_sample5000
-python inference_one_step.py -n 10000 -c 16 -w 100000
+#$ -N chain16_sample10000
+python inference_one_step_fixed_noise.py -n 10000 -c 16 -w 100000
