85bcd2258c75c58b2a32e775fbe8973c820dd372
mahdikooshkbaghi <mahdi.kooshkbaghi@gmail.com>
2021-09-14 09:35:47 -0400

    change elzar sampling number

--- elzar_submit.sh
+++ elzar_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -cwd
 #$ -l m_mem_free=16G
-#$ -pe threads 16
+#$ -pe threads 4
 #$ -o output
 #$ -e log
 #$ -N chain16_sample10000
-python inference_one_step_fixed_noise.py -n 2000 -c 16 -w 4000
+python inference_one_step_fixed_noise.py -n 5000 -c 4 -w 1000
